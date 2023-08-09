b59e8836f79e3afced70134d9c6b26981c3f6f08
eugenos-programos <elena.samchuk.73@mail.ru>
2023-08-09 23:29:46 +0300

    fix: fix assigning values for IterVariable

--- src/variables/ColumnVariable.py
+++ src/variables/ColumnVariable.py
@@ -7,15 +7,6 @@ class ColumnVariable(IterVariable):
 
     def __init__(self, elements: tuple, size: NumbVariable, builder: ir.builder.IRBuilder, ptr=None) -> None:
         super().__init__(elements, size, builder, ptr)
-
-    def set_value(self, value):
-        self.size = value.size
-        self.type = value.type
-        self.var = value.var
-        self.compile_init()
-
-    def get_value(self):
-        return self.ptr
-
+        
     def copy_variable(self, builder):
         return ColumnVariable(self, self.size, builder)
--- src/variables/IterVariable.py
+++ src/variables/IterVariable.py
@@ -48,6 +48,12 @@ class IterVariable(Variable):
             self.builder.store(result, self.ptr)
             self.ptr = self.builder.load(self.ptr)
 
+    def set_value(self, value):
+        self.var = value.var
+        self.size = value.size
+        self.ptr = value.ptr
+
     def get_value(self):
         return self.ptr
+
     
